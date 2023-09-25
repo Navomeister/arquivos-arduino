92bd16261b62df8a576c4547c0f3cbd6f47f18d7
Navo <110341514+Navomeister@users.noreply.github.com>
2023-09-25 17:13:14 -0300

    mudança na leitura da resposta

--- api_teste/request.js
+++ api_teste/request.js
@@ -1,14 +1,18 @@
 const form = document.getElementById('formulario');
 const responseDiv = document.querySelector('#resposta');
+const ctx = new AudioContext();
+let audio;
+
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const usuario = document.getElementById('usuario').value;
-    const sala = document.getElementById('numero_sala').value;
+    const endpoint = document.getElementById('numero_sala').value;
 
-    // fazer uma solicitação HTTP para a API
-    fetch('http://localhost/api-arduino/api.php/?endpoint=salas&usuario=' + usuario + '&numero_sala=' + sala, {
+    if (endpoint != "salas") {
+        // fazer uma solicitação HTTP para a API
+    fetch('https://apenasumtestezinho.000webhostapp.com/api/api.php/?endpoint=salas&usuario=' + usuario + '&endpoint=' + endpoint, {
         // mode: 'no-cors',
         headers: {
         "Content-Type": "application/json"
@@ -16,11 +20,33 @@ form.addEventListener('submit', function (event) {
     })
         .then(Response => Response.json()) // converte a resposta em json
         .then(data => {
-            responseDiv.textContent = JSON.stringify(data, null, 2);
+            responseDiv.textContent = toString(data);
         })
         .catch(error => {
             responseDiv.textContent = 'deu erro :/ ' + error.message;
         });
-    
-
+    } else {
+        // fazer uma solicitação HTTP para a API
+        fetch('https://apenasumtestezinho.000webhostapp.com/api/api.php/?endpoint=salas&usuario=' + usuario + '&endpoint=' + endpoint, {
+            // mode: 'no-cors',
+            headers: {
+            "Content-Type": "application/json"
+            }
+        })
+            .then(data => data.arrayBuffer())
+            .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+            .then(decodedAudio => {
+                audio = decodedAudio;
+                responseDiv.innerHTML = '<img src="images/play.svg" alt="" id="play">';
+                const play = document.getElementById("play");
+                play.addEventListener("mousedown", playback);
+            });
+    }
 });
+
+function playback() {
+    const playSound = ctx.createBufferSource();
+    playSound.buffer = audio;
+    playSound.connect(ctx.destination);
+    playSound.start(ctx.currentTime);
+  }
